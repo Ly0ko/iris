6f4ffd0d1c468de4c6da5b701abc97eb2bbdc9a9
ly0ko <ly0kodev@gmail.com>
2023-04-15 17:08:27 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,67 @@
+# IrisAI
+
+## Prerequisites
+
+- Python > 3.6 and < 3.10
+- DeepSpeech model files: deepspeech-model.pbmm and deepspeech-scorer.scorer
+- OpenAI API key
+
+You can get the model and scorer here:
+- Model file (.pbmm): https://github.com/mozilla/DeepSpeech/releases/download/v0.9.3/deepspeech-0.9.3-models.pbmm
+- Scorer file (.scorer): https://github.com/mozilla/DeepSpeech/releases/download/v0.9.3/deepspeech-0.9.3-models.scorer
+
+## Setup
+
+1. Clone the repository:
+
+```
+git clone https://github.com/ly0xr/iris.git
+cd iris
+```
+
+2. Create a virtual environment and activate it:
+
+### For Linux/macOS:
+
+```
+python3 -m venv venv
+source venv/bin/activate
+```
+
+### For Windows (cmd.exe):
+
+```
+python -m venv venv
+venv\Scripts\activate.bat
+```
+
+### For Windows (PowerShell):
+
+```
+python -m venv venv
+venv\Scripts\Activate.ps1
+```
+
+3. Install the required packages:
+
+`pip install -r requirements.txt`
+
+4. Place the DeepSpeech model files deepspeech-model.pbmm and deepspeech-scorer.scorer in the project directory.
+
+5. Set up the OpenAI API key:
+
+Create a .env file in the project directory and add the following line:
+
+`OPENAI_API_KEY=your-api-key-here`
+
+Replace your-api-key-here with your actual OpenAI API key.
+
+6. Run the voice assistant:
+
+`python irisai.py`
+
+The voice assistant will start listening for your voice commands. To exit, press Ctrl+C.
+
+## Troubleshooting
+
+If you encounter issues with the microphone device index, you can list available input devices using the list_input_devices() function in the script. Update the input_device_index parameter in the audio.open() function accordingly.
---
+++ irisai.py
@@ -0,0 +1,121 @@
+import os
+from dotenv import load_dotenv
+import deepspeech
+import numpy as np
+import pyaudio
+import requests
+import audioop
+import pyttsx3
+
+load_dotenv()
+
+api_key = os.environ.get("OPENAI_API_KEY")
+
+if not api_key:
+    raise ValueError("The OPENAI_API_KEY environment variable is not set.")
+
+headers = {"Authorization": f"Bearer {api_key}"}
+
+# Load the DeepSpeech model
+model_path = os.path.join(os.getcwd(), 'deepspeech-model.pbmm')
+scorer_path = os.path.join(os.getcwd(), 'deepspeech-scorer.scorer')
+model = deepspeech.Model(model_path)
+model.enableExternalScorer(scorer_path)
+
+with open("system_prompt.txt", "r") as file:
+    system_prompt = file.read()
+
+
+messages = [{"role": "system", "content": system_prompt}]
+
+
+# Set up audio recording
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 16000
+CHUNK = 1024
+
+# Initialize the PyAudio library
+audio = pyaudio.PyAudio()
+
+
+# Use this to find out the input device ID of the microphone you want to use
+def list_input_devices():
+    info = audio.get_host_api_info_by_index(0)
+    num_devices = info.get('deviceCount')
+    for i in range(0, num_devices):
+        if (audio.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+            print("Input Device ID", i, "-",
+                  audio.get_device_info_by_host_api_device_index(0, i).get('name'))
+
+
+def get_gpt3_response(messages):
+    data = {
+        "model": "gpt-3.5-turbo",
+        "messages": messages,
+    }
+
+    response = requests.post(
+        "https://api.openai.com/v1/chat/completions",
+        json=data,
+        headers=headers,
+    )
+
+    response.raise_for_status()
+    response_json = response.json()
+    return response_json["choices"][0]["message"]["content"].strip()
+
+
+def record_audio():
+    stream = audio.open(format=FORMAT, channels=CHANNELS,
+                        rate=RATE, input=True, frames_per_buffer=CHUNK, input_device_index=4)
+    frames = []
+    speech_detected = False
+    print("Listening...")
+
+    while True:
+        data = stream.read(CHUNK, exception_on_overflow=False)
+        rms = audioop.rms(data, 2)  # Compute RMS of the audio data
+
+        is_speech = rms > 100  # Set a threshold to determine if speech is detected
+
+        if is_speech:
+            frames.append(data)
+            speech_detected = True
+        elif speech_detected and not is_speech:
+            break
+
+    stream.stop_stream()
+    stream.close()
+    return b"".join(frames)
+
+
+def speak(text):
+    engine = pyttsx3.init()
+    # Set the speech rate; you can change this for faster or slower speech
+    engine.setProperty('rate', 200)
+    # Set the voice; you can change the index for a different voice
+    engine.setProperty('voice', engine.getProperty('voices')[1].id)
+    engine.say(text)
+    engine.runAndWait()
+
+
+# Main loop
+while True:
+    try:
+        audio_data = record_audio()
+        audio_data_np = np.frombuffer(audio_data, dtype=np.int16)
+        text = model.stt(audio_data_np)
+
+        if text.strip():
+            messages.append({"role": "user", "content": text})
+            gpt3_response = get_gpt3_response(messages)
+            messages.append({"role": "assistant", "content": gpt3_response})
+
+            print(f"You: {text}")
+            print(f"Iris: {gpt3_response}")
+            speak(gpt3_response)
+
+    except KeyboardInterrupt:
+        print("Exiting...")
+        break
